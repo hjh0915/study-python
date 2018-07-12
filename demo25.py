1d098488af0b6edaa9852bc2894a4e52b1181893
Gogs <gogs@fake.local>
2018-07-12 10:37:36 +0800

    再次练习乘法表（用while和format）

--- demo25.py
+++ demo25.py
@@ -34,24 +34,47 @@ def multiply_form(n):
     for i in numbers:
         s = ''
         for j in numbers:
-            s = s + ' '*4 + ('%d x %d = %2d' % (i, j, i*j))
+            s = s + ' '*4 + ('{0:d}x{1:d}={2:2d}'.format(i, j, i*j))
         print s 
 
 def multiply_triangle(n):
     for i in range(1, 10):
         s = ''
         for j in range(1, i + 1):
-            s = s + ' '*4 + ('%dx%d=%2d' % (i, j, i*j))
+            s = s + ' '*4 + ('{0:d}x{1:d}={2:2d}'.format(i, j, i*j))
         print s 
 
 def multiply_reverse(n):
     for i in range(9, 0, -1):
         s = ''
         for j in range(1, i + 1):
-            s = s + ' '*4 + ('%dx%d=%2d' % (i, j, i*j))
+            s = s + ' '*4 + ('{0:d}x{1:d}={2:2d}'.format(i, j, i*j))
         print s
             
-            
+def multiply_01(n):
+    i = 1
+    while i < 10:
+        s = ''
+        j = 1 
+        while j <= i:
+            s = s + ' '*4 + ('{0:d}x{1:d}={2:2d}'.format(i, j, i*j))
+            j = j + 1
+        i = i + 1
+        print s
+
+def multiply_02(n):
+    i = 9
+    while i >= 1:
+        s = ''
+        j = 1
+        while j <= i:
+            s = s + ' '*4 + ('{0:d}x{1:d}={2:2d}'.format(i, j, i*j))
+            j = j + 1
+        i = i - 1
+        print s 
+
+
+
     
            
 
@@ -64,6 +87,12 @@ if __name__ == '__main__':
     print multiply_triangle(9)
 
     print multiply_reverse(9)
+
+    print multiply_01(9)
+
+    print multiply_02(9)
+
+
 
 
 
