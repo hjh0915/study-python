2ded2e86730360a8764fe77fa225c637b6127f7c
hjh <623299157@qq.com>
2018-07-18 18:50:08 +0800

    测试十进制转二进制

---
+++ test_demo41.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+
+import unittest
+import demo41 
+
+class TestDemo41(unittest.TestCase):
+    """测试十进制转化为二进制"""
+    
+    def test_even_number(self):
+        """测试偶数"""
+
+        expected = '1100'
+        result = demo41.change(12)
+        self.assertEqual(expected, result)
+
+    def test_odd_number(self):
+        """测试奇数"""
+
+        expected = '1011'
+        result = demo41.change(11)
+        self.assertEqual(expected, result)
+        
+
+unittest.main()
+    
+
